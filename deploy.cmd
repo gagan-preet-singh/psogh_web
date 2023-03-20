47c32e221e2c28c66b4f8861403adf6f89718cdf
Gagan Preet Singh <gagan.preet.singh@hp.com>
2023-03-20 18:17:36 -0500

    change source to video - home

---
+++ deploy.cmd
@@ -0,0 +1,2 @@
+ng build --prod --base-href "https://punjabisociety.net"
+npx angular-cli-ghpages --dir=dist/psh_web
--- src/app/body/home/home.component.ts
+++ src/app/body/home/home.component.ts
@@ -17,6 +17,8 @@ export class HomeComponent implements OnInit {
   playInterval = 5000;
   autoPlay = true;
   thumb = false;
+  play = true;
+  pause = false;
 
   constructor(private _gallery: Gallery) {
     console.log(this._gallery);
@@ -28,36 +30,42 @@ export class HomeComponent implements OnInit {
     //   src: 'https://drive.google.com/uc?id=' + image_id,
     //   thumb: 'https://drive.google.com/uc?id=' + image_id
     // });
-    galleryRef.addImage({
-      src: 'assets/img/home_banner.jpg',
-      thumb: 'assets/img/home_banner.jpg',
-      title: 'Uniting Punjabis, Strengthening Communities'
-    });
-    galleryRef.addImage({
-      src: 'assets/img/home_banner.jpg',
-      thumb: 'assets/img/home_banner.jpg',
-      title: 'Preserving Our Heritage, Building Our Future'
-    });
-    galleryRef.addImage({
-      src: 'assets/img/home_banner.jpg',
-      thumb: 'assets/img/home_banner.jpg',
-      title: 'Celebrating Culture, Empowering People'
-    });
-    galleryRef.addImage({
-      src: 'assets/img/home_banner.jpg',
-      thumb: 'assets/img/home_banner.jpg',
-      title: 'From Punjab to Houston, Together We Thrive'
-    });
-    galleryRef.addImage({
-      src: 'assets/img/home_banner.jpg',
-      thumb: 'assets/img/home_banner.jpg',
-      title: 'Bridging Cultures, Building Bonds'
-    });
-    galleryRef.addImage({
-      src: 'assets/img/home_banner.jpg',
-      thumb: 'assets/img/home_banner.jpg',
-      title: 'One Community, One Voice, One Vision'
-    });
+    // galleryRef.addImage({
+    //   src: 'assets/img/home_banner.jpg',
+    //   thumb: 'assets/img/home_banner.jpg',
+    //   title: 'Uniting Punjabis, Strengthening Communities'
+    // });
+    // galleryRef.addImage({
+    //   src: 'assets/img/home_banner.jpg',
+    //   thumb: 'assets/img/home_banner.jpg',
+    //   title: 'Preserving Our Heritage, Building Our Future'
+    // });
+    // galleryRef.addImage({
+    //   src: 'assets/img/home_banner.jpg',
+    //   thumb: 'assets/img/home_banner.jpg',
+    //   title: 'Celebrating Culture, Empowering People'
+    // });
+    // galleryRef.addImage({
+    //   src: 'assets/img/home_banner.jpg',
+    //   thumb: 'assets/img/home_banner.jpg',
+    //   title: 'From Punjab to Houston, Together We Thrive'
+    // });
+    // galleryRef.addImage({
+    //   src: 'assets/img/home_banner.jpg',
+    //   thumb: 'assets/img/home_banner.jpg',
+    //   title: 'Bridging Cultures, Building Bonds'
+    // });
+    // galleryRef.addImage({
+    //   src: 'assets/img/home_banner.jpg',
+    //   thumb: 'assets/img/home_banner.jpg',
+    //   title: 'One Community, One Voice, One Vision'
+    // });
+    galleryRef.addVideo({
+      src: [{ url: 'https://drive.google.com/uc?id=1DJuyI-uqzj3di1RvlL4bE0wV8T81_EYf', type: 'video/mp4' }] as any,
+      disablePictureInPicture: true,
+      controls: false,
+      autoplay: true,
+    })
 
   }
 
